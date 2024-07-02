02389548f95fdd7a8f46b0e1d0e02a704e3de04c
venjerin <vldvrb@bk.ru>
2024-07-02 17:40:26 +0300

    BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import s from './App.module.css'
-import { Main } from './pages/Main/Main';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import s from "./App.module.css";
+import { Main } from "./pages/Main/Main";
 
 function App() {
   return (
-    <div className={s.app}>
-      <Main></Main>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main></Main>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
