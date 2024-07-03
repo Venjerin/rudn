51547f2873cd3279300db0ddc0672ab75f8e6247
venjerin <vldvrb@bk.ru>
2024-07-03 20:31:55 +0300

    static popup

--- src/components/PopupMenu/PopupMenu.jsx
+++ src/components/PopupMenu/PopupMenu.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import s from "./PopupMenu.module.css";
 import closeMenuIcon from "../../assets/images/navBarImages/menu-escape.svg";
+import backgroundImg from "../../assets/images/navBarImages/popup-backgroung-img.svg";
 
 export const PopupMenu = ({ isMenuOpen, setMenuOpen }) => {
   const handleToggle = () => {
     setMenuOpen(false);
   };
   return (
-    <div className={`${s.popup_menu} ${isMenuOpen? s.popup_menu_active : ''}`}>
+    <div className={`${s.popup_menu} ${isMenuOpen ? s.popup_menu_active : ""}`}>
       <div className={s.popup_menu_list}>
-        <img src={closeMenuIcon} alt="Закрыть меню" className={s.nav_close} onClick={handleToggle}/>
+        <img
+          src={closeMenuIcon}
+          alt="Закрыть меню"
+          className={s.nav_close}
+          onClick={handleToggle}
+        />
         <ul className={s.nav_list_popup}>
           <li>Главная</li>
           <li>РУДН</li>
