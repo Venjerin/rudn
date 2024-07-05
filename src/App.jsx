90e654363673bcc12fe2401ca3e0114ec0bcb744
venjerin <vldvrb@bk.ru>
2024-07-05 15:15:03 +0300

    Education page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Main } from "./pages/Main/Main";
 import { Rudn } from "./pages/Rudn/Rudn";
 import Error404 from "./pages/Error404/Error404";
 import { Science } from "./pages/Science/Science";
+import { Education } from "./pages/Education/Education";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Route path="/rudn" element={<Rudn></Rudn>}></Route>
         <Route path="/employees" element={<Error404></Error404>}></Route>
         <Route path="/science" element={<Science></Science>}></Route>
-        <Route path="/education" element={<Error404></Error404>}></Route>
+        <Route path="/education" element={<Education></Education>}></Route>
         <Route path="/clinic" element={<Error404></Error404>}></Route>
         <Route path="/facial-prosthetic" element={<Main></Main>}></Route>
         <Route path="*" element={<Error404></Error404>}></Route>
