ab0f8d15eb26f73ed9b81ec2e7ed3a88adee88f3
venjerin <vldvrb@bk.ru>
2024-07-16 17:58:55 +0300

    UserController

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 import express from "express";
 import dotenv from "dotenv";
-import { User } from "./models/User.js";
 import { registerValidation } from "./validations/auth.js";
-import { validationResult } from "express-validator";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import { checkAuth } from "./utils/checkAuth.js";
+import * as UserController from './controllers/UserController.js'
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -27,102 +24,9 @@ mongoose
     console.error("Error connecting to MongoDB:", err);
   });
 
-app.post("/auth/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(404).json({
-        message: "Пользователь не найден",
-      });
-    }
-
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: "Неверный логин или пароль",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret-key",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (error) {
-    console.error("Error during user authorisation:", error);
-    res.status(500).json({ message: "Error during user authorisation" });
-  }
-});
-
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new User({
-      email: req.body.email,
-      passwordHash: hash,
-    });
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret-key",
-      {
-        expiresIn: "30d",
-      }
-    );
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (error) {
-    console.error("Error during user registration:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
-app.get("/auth/me", checkAuth, async (req, res) => {
-    try {
-      const user = await User.findById(req.userId);
-  
-      if (!user) {
-        return res.status(404).json({
-          message: "Пользователь не найден",
-        });
-      }
-      const { passwordHash, ...userData } = user._doc;
-  
-      res.json(userData);
-    } catch (error) {
-      console.error("Something went wrong:", error);
-      res.status(500).json({ message: "Нет доступа" });
-    }
-  });
+  app.post("/auth/login", UserController.login);
+  app.post("/auth/register", registerValidation, UserController.register);
+  app.get("/auth/me", checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
   if (err) {
