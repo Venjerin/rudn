bb7bbe5a91c9f8307f04b70d2285b02036f7bf5c
venjerin <vldvrb@bk.ru>
2024-07-19 02:35:11 +0300

    registration

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -3,16 +3,27 @@ import s from "./Registration.module.css";
 import returnButton from "../../assets/images/personalAccImages/return-button.svg";
 import backgroungImg from "../../assets/images/personalAccImages/background-logo.svg";
 import { useNavigate } from "react-router";
+import { useForm } from "react-hook-form";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchRegister, selectIsAuth } from "../../redux/slices/auth";
 
 export const Registration = () => {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth)
   const [fullName, setFullName] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
   const handleFullNameChange = (event) => {
     setFullName(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
   };
 
   const navigate = useNavigate();
@@ -22,13 +33,31 @@ export const Registration = () => {
     setPassword('');
   };
 
-  const handleOutsideSubmit = () => {
-    const form = document.getElementById("signUpForm");
-    if (form) {
-      // form.submit();
-      console.log('Отправка формы:', fullName, password);
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      fullName: 'Full Name',
+      email: 'text@text.ru',
+      password: '12345'
+    },
+    mode: 'onChange'
+  })
+
+  const onSubmit = async () => {
+    const values = {fullName, email, password}
+    const data = await dispatch(fetchRegister(values)).catch(err => console.log(err))
+
+    if(!data.payload){
+      return alert('Не удалось зарегистрироваться')
     }
-  };
+
+    if('token' in data.payload){
+      window.localStorage.setItem('token', data.payload.token)
+    }
+
+    if(isAuth){
+      navigate('/')
+    }
+  }
 
   return (
     <>
@@ -45,25 +74,37 @@ export const Registration = () => {
             <p className={s.title}>Регистрация</p>
           </div>
           <div>
-            <form id="signUpForm" className={s.form}>
+            <form onSubmit={handleSubmit(onSubmit)} id="signUpForm" className={s.form} >
               <input
+                {...register("fullName")}
                 type="text"
-                placeholder="ФИО"
+                placeholder="Введите имя пользователя"
                 onChange={handleFullNameChange}
                 value={fullName}
               />
               <input
+                {...register("email")}
+                type="text"
+                placeholder="Почта"
+                onChange={handleEmailChange}
+                value={email}
+              />
+              <input
+                {...register("password")}
                 type="password"
                 placeholder="Пароль"
                 onChange={handlePasswordChange}
                 value={password}
               />
+              <button
+                type="submit"
+                className={s.registration_button}
+              >
+                Регистрация
+              </button>
             </form>
           </div>
         </div>
-        <button className={s.registration_button} onClick={handleOutsideSubmit}>
-          Регистрация
-        </button>
       </div>
     </>
   );
